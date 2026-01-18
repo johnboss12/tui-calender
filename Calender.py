ca1e701451063360510af8898b6fadf0a505cd2a
johnboss12 <trevor.lockwich2@d11.org>
2026-01-18 02:30:41 -0800

    Add files via upload
    initial code

---
+++ Calender.py
@@ -0,0 +1,75 @@
+import calendar
+import datetime
+import os
+from colorama import Back, init
+
+init(convert=True)
+
+now = datetime.datetime.now()
+month = calendar.monthcalendar(now.year,now.month);
+for i in range (len(month[0])):
+    if month[0][i]!=0:monthStart=i; break;
+hovered = ""
+today= ""
+selected =[0,monthStart]
+def drawCalender():
+    cal=   "  __________________________________\n /"+calendar.month_name[now.month]
+    for i in range (34-len(calendar.month_name[now.month])):cal+= " "
+    cal += "\\\n |_Mo_|_Tu_|_We_|_Th_|_Fr_|_Sa_|_Su_|\n"
+    for w in range (len(month)):
+        for d in range (len(month[w])):
+            if w==selected[0] and d==selected[1]: hovered=Back.GREEN
+            else: hovered=""
+            if w==now.day//7 and d==(now.day%7)+2: today=Back.CYAN
+            else: today=""
+            if d ==(len(month[w])-1):
+                    if month[w][d] == 0:cal+=" |    |\n" ;break
+                    if(month[w][d])<10:cal+=" |  " +today+hovered+str(month[w][d])+Back.BLACK+ " |\n" ;break
+                    cal+=" | "+today+hovered+str(month[w][d])+Back.BLACK + " |\n" ;break        
+            if month[w][d] == 0 and w==0: cal += " |   "
+            else:
+                if month[w][d] <10:
+                    cal+=" |  " +today+hovered+str(month[w][d])+Back.BLACK
+                else:
+                    cal+=" | "+today+hovered+str(month[w][d])+Back.BLACK
+    cal+=" \\__________________________________/"
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(cal)
+drawCalender()
+def getDate():
+    date = input("enter a day, q)uit e)dit\n")
+    try:
+        date=int(date)
+        return date
+    except:
+        if date=='q':exit();
+        if date=='e':os.system("start notepad.exe '%userprofile%\\Documents\\calendar.txt'" if os.name =='nt' else "nvim ~/Documents/calender.txt")
+        return getDate()
+    
+    #I know this is super stupid
+def singleDate(num):
+    if num<10:
+        num=str(("{:02d}".format(num)))
+        return num
+    else:
+        return num
+def moveCursor(Intdate):
+        selected[0] = (Intdate+monthStart-1)//7
+        selected[1]=  (Intdate+monthStart-1)%7
+        drawCalender()
+
+    
+    
+while True:
+    date=getDate()
+    moveCursor(date)
+    #print((str(now).split("-")[0])+(str(now).split("-")[1])  )
+    os.system('grep '+(str(now).split("-")[0])+"-"+(str(now).split("-")[1])+"-"+str(singleDate(date))+' %userprofile%\\Documents\\calendar.txt' if os.name=='nt' else 'grep '+(str(now).split(" ")[0])+ '~/Documents/calendar.txt')
+
+       
+#Add highlighting for days with @ tags
+#Add config/options
+#Add being able to see other months
+#Add todo veiw on the right or below the calender
+#Add proper editing
+#make sure things work on other OSes
